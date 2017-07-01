31823dab718653b78db6f2e3e19d3e8a9b22b5cd
Piotr Czarny <pecet1@gmail.com>
2017-07-01 21:13:14 +0200

    Parse tweet texts, search for hashtags, user mentions, regular words

--- twitter_json_to_sqlite.py
+++ twitter_json_to_sqlite.py
@@ -7,19 +7,22 @@ Author  : Piotr Czarny
 
 TODO: Store parsed files in db, so next time parsing is started, we only parse new files
 TODO: Also store db version or something in db, so we will force parsing if DB format is not current
-      (after confirmation)
+      (after confirmation), or maybe in future we will migrate old db tables to new db format,
+      via ALTER etc.
 """
 import sys
 import sqlite3
 import json
 import glob
 import time
+import collections
 
 class TwitterJsonToSqliteConverter(object):
     """ Class which allows you to convert JSON files to SQlite db """
     def __init__(self, database_filename='tweets.sqlite'):
         self.database = sqlite3.connect(database_filename)
         self.database_cursor = self.database.cursor()
+        self._create_database_tables()
 
     def __del__(self):
         if self.database:
@@ -39,7 +42,11 @@ class TwitterJsonToSqliteConverter(object):
             text TEXT,
             created_at TEXT,
 
-            -- reply?
+            -- reply? - store replies got directly from JSON
+            -- note that, if there are multiple users in reply only one will be included
+            -- that is why we need separate tweet_replies table
+            -- and that is why we need to parse tweet text itself
+            -- however we still include what JSON directly returns in SQLite because why not?
             in_reply_to_status_id INTEGER,
             in_reply_to_status_id_str TEXT,
             in_reply_to_user_id INTEGER,
@@ -54,6 +61,65 @@ class TwitterJsonToSqliteConverter(object):
         )
         """)
 
+        self.database_cursor.execute(""" CREATE TABLE IF NOT EXISTS tweet_replies(
+
+            -- ids
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tweet_id INTEGER NOT NULL,
+
+            -- reply to?
+            screen_name TEXT NOT NULL
+
+        )
+        """)
+
+        self.database_cursor.execute(""" CREATE TABLE IF NOT EXISTS tweet_hashtags(
+
+            -- ids
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tweet_id INTEGER NOT NULL,
+            hashtag TEXT NOT NULL
+
+        )
+        """)
+
+        self.database_cursor.execute(""" CREATE TABLE IF NOT EXISTS tweet_words(
+
+            -- ids
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tweet_id INTEGER NOT NULL,
+            word TEXT NOT NULL
+
+        )
+        """)
+
+
+    @classmethod
+    def _remove_interpunction(cls, input_str):
+        chars_to_remove = '.,:;/'
+        output_str = input_str
+        for char in chars_to_remove:
+            output_str = output_str.replace(char, '')
+        return output_str
+
+    @classmethod
+    def _deep_parse_text(cls, input_str):
+        mentions = []
+        hashtags = []
+        words = [] # regular words, excluding mentioned above
+        for word in input_str.split(' '):
+            word = cls._remove_interpunction(word.lower())
+            if len(word) >= 2: # ignore very short words
+                if word[0] == '@':
+                    mentions.append(word[1:])
+                elif word[0] == '#':
+                    hashtags.append(word[1:])
+                else:
+                    words.append(word)
+
+        return_type = collections.namedtuple('ParsedText', ['mentions', 'hashtags', 'words'])
+        return return_type(mentions=mentions, hashtags=hashtags, words=words)
+
     def parse_file(self, file_name):
         """ Parse JSON file and put parset results into SQLite DB """
         file_name_without_ext = file_name[file_name.rfind('/') + 1:file_name.rfind('.')]
@@ -67,7 +133,7 @@ class TwitterJsonToSqliteConverter(object):
         counter = 0
         for data in json_obj:
             counter += 1
-            if counter % 20 == 1 or counter == len(json_obj):
+            if counter % 40 == 1 or counter == len(json_obj):
                 print '      Parsed {0} of {1} tweets ({2}%)'.format(
                     counter, len(json_obj), round(float(counter) / float(len(json_obj)) * 100, 1)
                     )
@@ -93,10 +159,33 @@ class TwitterJsonToSqliteConverter(object):
                           source_parsed
                          )
 
+            parsed_text = self._deep_parse_text(data.get('text'))
+
             self.database_cursor.execute(""" INSERT INTO tweets (
                 id, id_str, text, created_at, in_reply_to_status_id, in_reply_to_status_id_str, in_reply_to_user_id, in_reply_to_user_id_str, in_reply_to_screen_name, source, source_parsed
             )
             VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", tweets_obj)
+
+            for mention in parsed_text.mentions:
+                self.database_cursor.execute(""" INSERT INTO tweet_replies (
+                    tweet_id, screen_name
+                )
+                VALUES (?, ?)
+                """, (data.get('id'), mention))
+
+            for hashtag in parsed_text.hashtags:
+                self.database_cursor.execute(""" INSERT INTO tweet_hashtags (
+                    tweet_id, hashtag
+                )
+                VALUES (?, ?)
+                """, (data.get('id'), hashtag))
+
+            for word in parsed_text.words:
+                self.database_cursor.execute(""" INSERT INTO tweet_words (
+                    tweet_id, word
+                )
+                VALUES (?, ?)
+                """, (data.get('id'), word))
 
             try:
                 self.database.commit()
@@ -107,7 +196,6 @@ class TwitterJsonToSqliteConverter(object):
 
     def parse_dir(self, dir_name):
         """ Iterate through JSON files in chosen directory and parse them """
-        self._create_database_tables()
         for file_name in glob.glob(dir_name + '*.js'):
             file_name = file_name.replace('\\', '/')
             print 'Parsing ' + file_name
@@ -115,8 +203,9 @@ class TwitterJsonToSqliteConverter(object):
 
 def main():
     """ Main method """
-    data_dir = 'tweets/data/js/tweets/'
-    TwitterJsonToSqliteConverter().parse_dir(data_dir)
+    #data_dir = 'tweets/data/js/tweets/'
+    #TwitterJsonToSqliteConverter().parse_dir(data_dir)
+    TwitterJsonToSqliteConverter().parse_file('tweets/data/js/tweets/2016_11.js') #just for testing
 
 if __name__ == "__main__":
     sys.exit(main())
