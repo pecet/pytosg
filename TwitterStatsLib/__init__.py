0f4150a67cdc657cc256ad6466600fc2883f5fdc
Piotr Czarny <pecet1@gmail.com>
2019-08-13 13:10:43 +0200

    Start python 3.7 rewrite with fresh dir

--- TwitterStatsLib/__init__.py
+++
@@ -1,7 +0,0 @@
-""" Main utility module """
-
-from .LazyDict import LazyDict
-from .Output import Output, HTMLOutput
-from .TwitterStatsGenerator import TwitterStatsGenerator
-from .TwitterJsonToSqliteConverter import TwitterJsonToSqliteConverter
-from .InsertableOrderedDict import InsertableOrderedDict
--- TwitterStatsLib/test/__init__.py
+++
@@ -1,9 +0,0 @@
-""" Main unit test module """
-
-import unittest
-from .test_LazyDict import TestLazyDict
-from .test_TwitterStatsGenerator import TestMap
-from .test_InsertableOrderedDict import TestInsertableOrderedDict
-
-if __name__ == '__main__':
-    unittest.main()
